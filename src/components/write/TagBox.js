fdbe1ae477d4fcfad45fcc60cd5c1935581a2920
neo@zillinks.com <keunhey0107@gmail.com>
2022-08-31 04:35:10 +0900

    Tagbox

--- src/components/write/TagBox.js
+++ src/components/write/TagBox.js
@@ -4,4 +4,85 @@ import palette from "../../lib/styles/palette";
 
 const TagboxBlock = styled.div`
   width: 100%;
+  border-top: 1px solid ${palette.gray[2]};
+  padding-top: 2rem;
+
+  h4 {
+    color: ${palette.gray[8]};
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+  }
 `;
+
+const TagForm = styled.form`
+  border-radius: 4px;
+  overflow: hidden;
+  display: flex;
+  width: 256px;
+  border: 1px solid ${palette.gray[9]};
+  input,
+  button {
+    outline: none;
+    border: none;
+    font-size: 1rem;
+  }
+
+  input {
+    padding: 0.5rem;
+    flex: 1;
+    min-width: 0;
+  }
+  button {
+    cursor: pointer;
+    padding-right: 1rem;
+    padding-left: 1rem;
+    border: none;
+    background: ${palette.gray[8]};
+    color: white;
+    font-weight: bold;
+    &:hover {
+      background: ${palette.gray[6]};
+    }
+  }
+`;
+
+const Tag = styled.div`
+  margin-right: 0.5rem;
+  color: ${palette.gray[6]};
+  cursor: pointer;
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+const TagListBlock = styled.div`
+  display: flex;
+  margin-top: 0.5rem;
+`;
+
+// React.memo를 사용하여 tag 값이 바뀔 때만 리렌더링 되도록 처리
+const TagItem = React.memo(({ tag }) => <Tag>#{tag}</Tag>);
+
+// React.memo를 사용하여 tag 값이 바뀔 때만 리렌더링 되도록 처리
+const TagList = React.memo(({ tags }) => (
+  <TagListBlock>
+    {tags.map((tag) => (
+      <TagItem key={tag} tag={tag} />
+    ))}
+  </TagListBlock>
+));
+
+const TagBox = () => {
+  return (
+    <TagboxBlock>
+      <h4>태그</h4>
+      <TagForm>
+        <input placeholder="태그를 입력하세요" />
+        <button type="submit">추가</button>
+      </TagForm>
+      <TagList tags={["태그1", "태그2", "태그3"]} />
+    </TagboxBlock>
+  );
+};
+
+export default TagBox;
--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,10 +1,12 @@
 import Editor from "../components/write/Editor";
 import Responsive from "../components/common/Responsive";
+import TagBox from "../components/write/TagBox";
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
+      <TagBox />
     </Responsive>
   );
 };
